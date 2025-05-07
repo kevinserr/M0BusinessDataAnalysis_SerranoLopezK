72ceb811098f9fd553995ded9278528933cd3c5f
Kevin Serrano <kevinserrl2002@gmail.com>
2025-05-07 10:56:56 -0400

    fixed grammatical errors

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd # imports the pandas library
 
-# reads the csv files and stores it creates a pands.dataframe named vgsales
+# reads the csv file and creates a pandas.dataframe named vgsales
 vgsales = pd.read_csv('vgsales.csv')
 
 # Next 5 lines assigns the individual columns to different variables
@@ -10,12 +10,14 @@ EU_sales = vgsales['EU_Sales']
 Other_sales = vgsales['Other_Sales']
 Global_sales = vgsales['Global_Sales']
 
-# Creates a list that contains the sums of the individual region sales
-# Using string.format() to store only upto the second decimal place
+# Creates a list that contains the sums of the individual regional sales
+# Uses string.format() to store only up to the second decimal place
 sumByRegion = ["{:.2f}".format(NA_sales.sum()),"{:.2f}".format(JP_sales.sum()),
                "{:.2f}".format(EU_sales.sum()),"{:.2f}".format(Other_sales.sum()),
                "{:.2f}".format(Global_sales.sum())] 
+
 total = 0 # Initialize and assigns total to 0
+
 # for loop to find the total sum of all regional sales
 for i in sumByRegion:
     # converts the string i into a float and then an int in order to compute
