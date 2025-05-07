28c63e312d4d71fc19846f424237c70d46b2d2ad
Kevin Serrano <kevinserrl2002@gmail.com>
2025-05-07 13:51:49 -0400

    Added Graph

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import pandas as pd # imports the pandas library
 vgsales = pd.read_csv('vgsales.csv')
 
 # Next 5 lines assigns the individual columns to different variables
-NA_sales = vgsales['NA_Sales']
-JP_sales = vgsales['JP_Sales']
-EU_sales = vgsales['EU_Sales']
-Other_sales = vgsales['Other_Sales']
-Global_sales = vgsales['Global_Sales']
+naTotalSales = vgsales['NA_Sales'].sum()
+euTotalSales = vgsales['EU_Sales'].sum()
+jpTotalSales = vgsales['JP_Sales'].sum()
+globalTotalSales = vgsales['Global_Sales'].sum()
+otherTotalSales = vgsales['Other_Sales'].sum()
 
-# Creates a list that contains the sums of the individual regional sales
+# # Creates a list that contains the sums of the individual regional sales
 # Uses string.format() to store only up to the second decimal place
+<<<<<<< HEAD
 sumByRegion = [NA_sales.sum(), JP_sales.sum(),EU_sales.sum(),Other_sales.sum(),Global_sales.sum()] 
 
 total = 0 # Initialize and assigns total to 0
@@ -21,4 +22,11 @@ for i in sumByRegion:
 
     
 print(total)
+=======
+sumByRegion = [naTotalSales,euTotalSales,jpTotalSales,globalTotalSales,otherTotalSales] 
+
+total = 0 # Initialize and assigns total to 0
+
+print(sum(sumByRegion))
+>>>>>>> refs/remotes/origin/main
 
