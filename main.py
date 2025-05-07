5c29760663a89a8c97e17ffcaed70ad95a326b46
Kevin Serrano <kevinserrl2002@gmail.com>
2025-05-07 10:47:12 -0400

    Using Pandas I got the average per region and total number of sales

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import pandas as pd
 
-
-vgSales = pd.read_csv("vgsales.csv")
-
-print(vgSales.head(2))
+vgsales = pd.read_csv('vgsales.csv')
+NA_sales = vgsales['NA_Sales']
+JP_sales = vgsales['JP_Sales']
+EU_sales = vgsales['EU_Sales']
+Other_sales = vgsales['Other_Sales']
+Global_sales = vgsales['Global_Sales']
+sumByRegion = ["{:.2f}".format(NA_sales.sum()),"{:.2f}".format(JP_sales.sum()),
+               "{:.2f}".format(EU_sales.sum()),"{:.2f}".format(Other_sales.sum()),
+               "{:.2f}".format(Global_sales.sum())] 
+total = 0
+for i in sumByRegion:
+    total+= int(float(i))
+print(total)
 
