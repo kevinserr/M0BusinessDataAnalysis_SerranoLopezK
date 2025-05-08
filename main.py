06ec5db53ca1f3925540027a548d3d8584e7bf13
Kevin Serrano <kevinserrl2002@gmail.com>
2025-05-08 03:47:02 -0400

    Prints 5 Header, data types and Data Frame

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import matplotlib.pyplot as plt
 
 # reads the csv file and creates a pandas.dataframe named vgsales
 vgsales = pd.read_csv('data/vgsales.csv')
+pd.set_option('display.max_columns', None)
+
+print("First 5 Rows:")
+print(vgsales.head(5))
+print('Data Types for All Columns: \n')
+print(vgsales.head(0).dtypes)
 
 # Next 5 lines assigns the sum of each regional sale
 naTotalSales = vgsales['NA_Sales'].sum()
@@ -21,10 +27,11 @@ df = pd.DataFrame({'N.A': [naTotalSales],
                    'Other': [otherTotalSales]},
                    index=['Total Sales:'])
 
+print('Data Frame with region and their total sales','\n')
 print(df)
 
 color = ['lightblue', 'blue', 'purple', 'red']
 plt.bar(x_values, y_values, width = 0.5, color = color, align='center')
 plt.xlabel("Regions")
 plt.ylabel("Total (Millions)")
-plt.show()
+#plt.show()
